ec41f764536541d00d06759ddc36133cb12148b7
bhenrich <benjamin@bhenrich.de>
2023-12-21 13:59:16 +0100

    V1, frontend finalised, tag parsing and more

--- server/api.js
+++ server/api.js
@@ -1,50 +1,59 @@
-
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 const port = 5033;
 
 // Your SSL Certs here
 const credentials = {
-    key: fs.readFileSync('/etc/letsencrypt/live/yuniiworks.de/privkey.pem', 'utf8'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/yuniiworks.de/cert.pem', 'utf8'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/yuniiworks.de/chain.pem', 'utf8'),
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/yuniiworks.de/privkey.pem",
+    "utf8",
+  ),
+  cert: fs.readFileSync("/etc/letsencrypt/live/yuniiworks.de/cert.pem", "utf8"),
+  ca: fs.readFileSync("/etc/letsencrypt/live/yuniiworks.de/chain.pem", "utf8"),
 };
 
 // allow all and any cors requests
 app.use(cors());
 
-app.get('/servers', async (req, res) => {
-    console.log('Requesting servers');
+app.get("/servers", async (req, res) => {
+  console.log("Requesting servers");
 
-    const serverUrls = [
-        'https://leviathan.spacestation14.com/server/status',
-        'https://lizard.spacestation14.io/server/status',
-        'https://lizard.spacestation14.io/salamander/status'
-    ];
+  const serverUrls = [
+    "https://leviathan.spacestation14.com/server/status",
+    "https://lizard.spacestation14.io/server/status",
+    "https://leviathan.spacestation14.com/vulture/status",
+    "https://lizard.spacestation14.io/salamander/status",
+    "https://changelog.ss13.moe/ss14_server/status",
+    "https://miros.spacestation14.io/server/status",
+  ];
 
-    try {
-        const responses = await Promise.all(serverUrls.map(url => fetch(url)));
+  try {
+    const responses = await Promise.all(serverUrls.map((url) => fetch(url)));
 
-        const data = await Promise.all(responses.map(response => {
-            if (response.ok) {
-                return response.json(); // Only call .json() if the response is ok
-            } else {
-                throw new Error(`Error fetching ${response.url}: ${response.statusText}`);
-            }
-        }));
+    const data = await Promise.all(
+      responses.map((response) => {
+        if (response.ok) {
+          return response.json(); // Only call .json() if the response is ok
+        } else {
+          throw new Error(
+            `Error fetching ${response.url}: ${response.statusText}`,
+          );
+        }
+      }),
+    );
 
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
-    }
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(port, () => {
-    console.log(`Server running on https://yuniiworks.de:${port}`);
+  console.log(`Server running on https://yuniiworks.de:${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,90 +1,220 @@
-import logo from './logo.png';
-import './App.css';
+import "./App.css";
 
-import React, { useEffect, useState } from 'react';
-import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+import React, { useEffect, useState } from "react";
 
 function App() {
-	const [servers, setServers] = useState([]);
-	useEffect(() => {
-		fetch('https://yuniiworks.de:5033/servers')
-			.then(response => response.json())
-			.then(data => setServers(data))
-			.catch(error => console.log(error));
-	}, []);
+  const [servers, setServers] = useState([]);
+  const [refresh, setRefresh] = useState(0);
 
-	function calcRoundTimeLength(startTime, running) {
+  useEffect(() => {
+    if (refresh > 0) {
+      const interval = setInterval(() => {
+        fetchData();
+      }, refresh * 60000);
+      return () => clearInterval(interval);
+    }
 
-		if (running != 1) return "In the Lobby";
-		else {
-			const startTimeDate = new Date(startTime);
-			const currentTime = new Date();
+    fetchData();
+  }, [refresh]);
 
-			const timeDifference = currentTime.getTime() - startTimeDate.getTime();
+  function calcRoundTimeLength(startTime, running) {
+    if (running !== 1) return "In the Lobby";
+    else {
+      const startTimeDate = new Date(startTime);
+      const currentTime = new Date();
 
-			// return in format: 1 Hour, 23 Minutes, 45 Seconds
-			return `${Math.floor(timeDifference / 1000 / 60 / 60)} Hours, ${Math.floor(timeDifference / 1000 / 60) % 60} Minutes, ${Math.floor(timeDifference / 1000) % 60} Seconds`;
+      const timeDifference = currentTime.getTime() - startTimeDate.getTime();
 
-		}
-	}
+      // return in format: 1 Hour, 23 Minutes, 45 Seconds
+      return `${Math.floor(timeDifference / 1000 / 60 / 60)} Hours, ${
+        Math.floor(timeDifference / 1000 / 60) % 60
+      } Minutes, ${Math.floor(timeDifference / 1000) % 60} Seconds`;
+    }
+  }
 
-	return (
-		<div className="App">
-			<header className="header">
-				<div className="logo">
-					<img src={logo} className="headerLogo" alt="logo" />
-					<span className="headerText">Wizard's Den Server Dashboard</span>
-				</div>
+  const fetchData = async () => {
+    fetch("https://yuniiworks.de:5033/servers")
+      .then((response) => response.json())
+      .then((data) => setServers(data))
+      .catch((error) => console.log(error));
+  };
 
-				<div className="refreshButton">
-					<button onClick={() => window.location.reload(false)} className='refresh'>Refresh</button>
-				</div>
-			</header>
+  function getLanguageFullName(langCode) {
+    const displayNames = new Intl.DisplayNames(["en"], { type: "language" });
+    return displayNames.of(langCode.split(":")[1]);
+  }
 
-			<main>
-				<div className="cardContainer">
-					{
-						servers.map((server, index) => (
-							<div className='card' key={index}>
-								<div className='leftContainer'>
-									<div className="cardHeader">
-										<span className="cardTitle">{server.name}</span><br />
-										<span className="cardPlayers">{server.players} / {server.soft_max_players} Players</span><br />
-									</div>
-									<div className="cardBody">
-										<div className="cardStatus">
-											<span className="cardStatusText">{server.status}</span><br />
-										</div>
-										<div className="cardFooter">
-											<span className="serverMap">Station: {server.map ? server.map : "Error fetching Map"}</span><br />
-											<span className="serverRuntime">{calcRoundTimeLength(server.round_start_time, server.run_level)}</span><br />
-										</div>
-									</div>
-								</div>
-								<div className='rightContainer'>
-									<div className="cardFooter">
-										<span className="serverRoundNr">Shift Nr. #{server.round_id}</span><br />
-										<div className="serverTags">
-											{server.tags.map((tag, index) => (
-												<span className="serverTag" key={index}>
-													{tag}
-												</span>
-											))}
-										</div>
-									</div>
-								</div>
-							</div>
-						))
-					}
-				</div>
-			</main >
+  function translateRegion(regionTag) {
+    switch (regionTag) {
+      case "region:eu_w":
+        return "West Europe";
+      case "region:eu_e":
+        return "East Europe";
+      case "region:am_n_w":
+        return "North America West";
+      case "region:am_n_c":
+        return "North America Central";
+      case "region:am_n_e":
+        return "North America East";
+      case "region:am_c":
+        return "Central America";
+      case "region:am_s_s":
+        return "South America South";
+      case "region:am_s_e":
+        return "South America East";
+      case "region:am_s_w":
+        return "South America West";
+      case "region:af_n":
+        return "North Africa";
+      case "region:af_c":
+        return "Central Africa";
+      case "region:af_s":
+        return "South Africa";
+      case "region:me":
+        return "Middle East";
+      case "region:as_n":
+        return "North Asia";
+      case "region:as_se":
+        return "South East Asia";
+      case "region:as_e":
+        return "East Asia";
+      case "region:ind":
+        return "India";
+      case "region:oce":
+        return "Oceania";
+      default:
+        return "Unknown Region";
+    }
+  }
 
-			<footer>
-				<p className="footerText">Made with 💜 by YuNii</p>
-				<p className="footerText">✨ Play Space Station 14 ✨</p>
-			</footer>
-		</div >
-	);
+  function parseRPTag(rp) {
+    switch (rp) {
+      case "rp:low":
+        return "Low RP";
+      case "rp:med":
+        return "Medium RP";
+      case "rp:high":
+        return "High RP";
+      case "rp:none":
+        return "No RP";
+      default:
+        return "Unknown RP";
+    }
+  }
+
+  function parseTags(tags) {
+    const lang = tags.find((tag) => tag.includes("lang:"));
+    const region = tags.find((tag) => tag.includes("region:"));
+    const rp = tags.find((tag) => tag.includes("rp:"));
+    const langName = getLanguageFullName(lang);
+    const regionName = translateRegion(region);
+    const rpName = parseRPTag(rp);
+
+    return [langName, regionName, rpName];
+  }
+
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo">
+          <a href="https://spacestation14.io" target="_blank" rel="noreferrer">
+            <img src="logo.png" className="headerLogo" alt="logo" />
+          </a>
+          <span className="headerText">Wizard's Den Server Dashboard</span>
+        </div>
+      </header>
+
+      <div className="refreshButton">
+        <button onClick={fetchData} className="refresh">
+          Refresh
+        </button>
+        <span className="refreshText">Refresh every &nbsp;</span>
+        <select
+          className="refreshDropdown"
+          name="Automatic Refresh"
+          onChange={(e) => setRefresh(e.target.value)}
+        >
+          <option value="0" className="refreshDropdownInner">
+            Manual
+          </option>
+          <option value="1" className="refreshDropdownInner">
+            1 Minute
+          </option>
+          <option value="5" className="refreshDropdownInner">
+            5 Minutes
+          </option>
+          <option value="10" className="refreshDropdownInner">
+            10 Minutes
+          </option>
+        </select>
+      </div>
+      <main>
+        <div className="cardContainer">
+          {servers.map((server, index) => (
+            <div className="card" key={index}>
+              <div className="leftContainer">
+                <div className="cardHeader">
+                  <span className="cardTitle">{server.name}</span>
+                  <br />
+                  <span className="cardPlayers">
+                    {server.players} / {server.soft_max_players} Players
+                  </span>
+                  <br />
+                </div>
+                <div className="cardBody">
+                  <div className="cardStatus">
+                    <span className="cardStatusText">{server.status}</span>
+                    <br />
+                  </div>
+                  <div className="cardFooter">
+                    <span className="serverMap">
+                      Station: {server.map ? server.map : "Error fetching Map"}
+                    </span>
+                    <br />
+                    <span className="serverRuntime">
+                      {calcRoundTimeLength(
+                        server.round_start_time,
+                        server.run_level,
+                      )}
+                    </span>
+                    <br />
+                    <div className="serverTags">
+                      {parseTags(server.tags).map((tag, index) => (
+                        <span className="serverTag" key={index}>
+                          {tag}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rightContainer">
+                <div className="cardRoundNr">
+                  <span className="serverRoundNr">
+                    Shift #{server.round_id}
+                  </span>
+                  <br />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      <footer>
+        <p className="footerText">
+          <a href="https://github.bhenrich.de" target="_blank" rel="noreferrer">
+            Made with 💜 by YuNii
+          </a>
+        </p>
+        <p className="footerText">
+          <a href="https://spacestation14.io" target="_blank" rel="noreferrer">
+            ✨ Play Space Station 14 ✨
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
